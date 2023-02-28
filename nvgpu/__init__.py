a436bd98761415ad9ffaa2cd88086becfc86f62b
Rahul-Scalers <rahul@scalers.ai>
2023-02-27 18:00:12 -0800

    The gpu stats have been fine tuned for scalers benchmarking purposes

--- nvgpu/__init__.py
+++ nvgpu/__init__.py
@@ -1,14 +1,16 @@
 import re
 import six
 import subprocess
+import pynvml as nv
+from nvgpu.nvml import nvml_context
 
 
 def gpu_info():
     gpus = [line for line in _run_cmd(['nvidia-smi', '-L']) if line]
-    gpu_infos = [re.match('GPU ([0-9]+): ([^(]+) \(UUID: ([^)]+)\)', gpu) for gpu in gpus]
-    gpu_infos = [info.groups() for info in gpu_infos if info is not None]
+    gpu_infos = [re.match('GPU ([0-9]+): ([^(]+) \(UUID: ([^)]+)\)', gpu).groups() for gpu in gpus]
     gpu_infos = [dict(zip(['index', 'type', 'uuid'], info)) for info in gpu_infos]
     gpu_count = len(gpus)
+
 
     lines = _run_cmd(['nvidia-smi'])
     cuda_version = float(lines[2].split('CUDA Version: ')[1].split(' ')[0])
@@ -18,12 +20,19 @@ def gpu_info():
     else:
         line_distance = 4
         selected_lines = lines[8:8 + line_distance * gpu_count]
-    for i in range(gpu_count):
-        mem_used, mem_total = [int(m.strip().replace('MiB', '')) for m in
-                               selected_lines[line_distance * i + 1].split('|')[2].strip().split('/')]
-        gpu_infos[i]['mem_used'] = mem_used
-        gpu_infos[i]['mem_total'] = mem_total
-        gpu_infos[i]['mem_used_percent'] = 100. * mem_used / mem_total
+    with nvml_context():
+        for i in range(gpu_count):
+            mem_used, mem_total = [int(m.strip().replace('MiB', '')) for m in
+                                selected_lines[line_distance * i + 1].split('|')[2].strip().split('/')]
+            gpu_infos[i]['mem_used'] = mem_used
+            gpu_infos[i]['mem_total'] = mem_total
+            gpu_infos[i]['mem_used_percent'] = 100. * mem_used / mem_total
+
+            handle = nv.nvmlDeviceGetHandleByIndex(i)
+            device_name = nv.nvmlDeviceGetName(handle)
+            temperature = nv.nvmlDeviceGetTemperature(handle, nv.NVML_TEMPERATURE_GPU)
+            gpu_infos[i]['temp'] = temperature
+
 
     return gpu_infos
 
@@ -33,6 +42,7 @@ def _run_cmd(cmd):
     if six.PY3:
         output = output.decode('UTF-8')
     return output.split('\n')
+
 
 def available_gpus(max_used_percent=20.):
     return [gpu['index'] for gpu in gpu_info() if gpu['mem_used_percent'] <= max_used_percent]
